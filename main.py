ccec272ecc25d25e9991aab44ee7d911431446e8
practice <mica.maharshak@gmail.com>
2026-09-08 11:12:18 +0300

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-print("Hello World")
+import sys
+import time
+import pygame
+import consts
+import screen
+
+state_of_game={
+    "p_pos":(0,0)
+
+
+}
+def main():
+    game_state=consts.RUNNING_STATE
+    pygame.init()
+
+    while game_state==consts.RUNNING_STATE:
+        screen.draw_game(state_of_game)
+
+    return
+
+
+def end_game():
+    time.sleep(3)
+    pygame.quit()
+    return
 
 
 
+#==============================================================================
 
-
-
-
-
+if __name__ == "__main__":
+    main()
--- screen.py
+++ screen.py
@@ -1,12 +1,20 @@
 import pygame
+from sys import exit
 import consts
 
 pygame.init()
 window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
+pygame.display.set_caption("The Flag")#title
 while True:#game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
-
+    window.fill((63, 120, 11))
+    pygame.display.update()
+    background = pygame.image.load("background.png")
 
 
 
