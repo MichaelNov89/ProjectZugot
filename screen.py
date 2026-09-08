2249c6978330834985bdaded84519fc6acf39e8d
practice <mica.maharshak@gmail.com>
2026-09-08 11:15:41 +0300

    first commit

--- screen.py
+++ screen.py
@@ -4,17 +4,12 @@ import consts
 
 pygame.init()
 window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-
-pygame.display.set_caption("The Flag")#title
-while True:#game loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
+def screen_display():
+    pygame.display.set_caption("The Flag")#title
     window.fill((63, 120, 11))
     pygame.display.update()
     background = pygame.image.load("background.png")
+
 
 
 
